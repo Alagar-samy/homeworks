289f26e45d61d1872277053682aee67ee3fe6d7e
Alagar-samy <110388626+Alagar-samy@users.noreply.github.com>
2022-09-01 21:45:53 +0530

    Update merge string.py

--- week4/merge string.py
+++ week4/merge string.py
@@ -6,3 +6,13 @@ for index in range(0,len(input1)):
     #merge the characters of two input strings one by one
     output += input1[index] + input2[index]
 print("output ",output)
+
+test case 1:
+enter the 1st string = abcd
+enter the 2nd string = 1234
+output  a1b2c3d4
+ 
+test case 2:
+enter the 1st string = alagar
+enter the 2nd string = 123456
+output  a1l2a3g4a5r6

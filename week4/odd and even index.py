4034bde0d28a99f68c629fe48e15e83be9725b8a
Alagar-samy <110388626+Alagar-samy@users.noreply.github.com>
2022-09-02 17:54:22 +0530

    Update odd and even index.py

--- week4/odd and even index.py
+++ week4/odd and even index.py
@@ -8,4 +8,19 @@ print("even index characters in reverse order")
 for index in range(len(input1)-1,-1,-1):
     if(index % 2 ==0):
         print(input1[index],end =" ")
+   
+test case 1:
     
+enter the string = abcd
+odd index characters
+b d
+even index characters in reverse order
+c a
+
+test case 2:
+    
+enter the string = alagar
+odd index characters
+l g r
+even index characters in reverse order
+a a a

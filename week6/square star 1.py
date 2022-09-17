48cec42f88cf4741cd6791549021a6ff3a6d8ca6
Alagar-samy <110388626+Alagar-samy@users.noreply.github.com>
2022-09-17 17:01:07 +0530

    Add files via upload

---
+++ week6/square star 1.py
@@ -0,0 +1,8 @@
+width = int(input("enter the width of the square = "))
+for row in range(1,width+1):
+    for column in range(1,width+1):
+        if(row == 1 or row == width or column == 1 or column == width):
+            print("*",end= " ")
+        else:
+            print(" ",end= " ")
+    print()

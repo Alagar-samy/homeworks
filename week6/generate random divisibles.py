81caf5b76a357ea0395072b5c6e8b79d67f06055
Alagar-samy <110388626+Alagar-samy@users.noreply.github.com>
2022-09-18 10:31:52 +0530

    Add files via upload

---
+++ week6/generate random divisibles.py
@@ -0,0 +1,9 @@
+import random # to generate random numbers by import 'random' module
+number = int(input("enter the divisble number that you want to genrate = "))
+limit = int(input("enter the limit = "))
+def random_number(number,limit): # defining a function to generate random numbers divisible by the given input number
+    for i in range(1,limit+1): 
+        num = random.randint(1,limit+1)
+        if(num % number == 0): # to chech the random number is divisible by the given input number
+            print(num,end=" ")
+random_number(number,limit) # calling the function
